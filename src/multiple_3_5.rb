1a767d0f5aaea1513bad4afe0551722337703512
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-07 12:45:51 +0300

    refactor(multiple_3_5):  code according to comments
    Add tests, remove comments, use reduse instead of times, remove return, add space to the end of file

--- src/multiple_3_5.rb
+++ src/multiple_3_5.rb
@@ -1,28 +1,13 @@
-# def find_sum_of_multiples(num, last_num_in_range)
-#   sum = 0;
-#   multiple = ((last_num_in_range - 1) / num).floor * num
-#   while multiple >= num 
-#     next if multiple % num != 0
-#     puts multiple
-#     sum += multiple
-#     multiple -= num
-#   end
-
-#   return sum
-# end
-
-# def find_sum_of_sums
-#   mult_of_5_sum = find_sum_of_multiples(5, 1000)
-#   mult_of_3_sum = find_sum_of_multiples(3, 1000)
-#   return mult_of_5_sum + mult_of_3_sum
-# end
-
-def find_sum_loop
-  sum = 0
-  1000.times do |x|
-    sum += x if (x % 3 == 0) || (x % 5 == 0)
+module FindSumOfMultiples 
+  def self.find_sum_loop
+    (0...1000).reduce(0) do |sum, num|
+      if num % 3 == 0 || num % 5 == 0
+        sum = sum + num
+      else
+        sum
+      end
+    end
   end
-  return sum
 end
 
-puts find_sum_loop()
+puts FindSumOfMultiples.find_sum_loop
