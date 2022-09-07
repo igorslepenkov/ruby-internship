2d43e3cbc4c19ee8960cc07c979e3b703b79f09b
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-07 13:01:50 +0300

    refactor(multiple-3-5): use += and inline if statement

--- src/multiple_3_5.rb
+++ src/multiple_3_5.rb
@@ -1,11 +1,8 @@
 module FindSumOfMultiples 
   def self.find_sum_loop
     (0...1000).reduce(0) do |sum, num|
-      if num % 3 == 0 || num % 5 == 0
-        sum = sum + num
-      else
-        sum
-      end
+      sum += num if num % 3 == 0 || num % 5 == 0
+      sum
     end
   end
 end
