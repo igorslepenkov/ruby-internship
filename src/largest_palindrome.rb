dbc056df19455f4e7ddb1843ef79d5b866cba6eb
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-08 14:20:13 +0300

    rafactor(largest-palindrome): add spaces around -, remove ternar operator in palindrom? func, change is_palindrome to palindrome?

--- src/largest_palindrome.rb
+++ src/largest_palindrome.rb
@@ -1,17 +1,17 @@
 module PolindromeNumbers
-  def self.is_palindrome?(number)
-    number.to_s == number.to_s.reverse ? true : false
+  def self.palindrome?(number)
+    number.to_s == number.to_s.reverse
   end
 
   def self.find_largest_palindrome(num_of_digits)
-    lower_limit = 10 ** (num_of_digits-1)
+    lower_limit = 10 ** (num_of_digits - 1)
     max_num = (10 ** num_of_digits) - 1
     largest_palindrome = 0;
 
     (lower_limit..max_num).each do |i|
       (lower_limit..max_num).each do |j|
         mult = i * j
-        next if !self.is_palindrome?(mult)
+        next if !self.palindrome?(mult)
         if largest_palindrome < mult
           largest_palindrome = mult
         end
