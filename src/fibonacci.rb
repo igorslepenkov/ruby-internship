691636bc7140d0e0ff46f02b4ee69d749d56277c
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-08 13:55:19 +0300

    refactor(fibonacci): use methods last and sum, remove assignment of sum

--- src/fibonacci.rb
+++ src/fibonacci.rb
@@ -1,10 +1,10 @@
 module FibonacciNumbers
   def self.get_sum_of_even
     arr = [0, 1]
-    while arr[-1] + arr[-2] < 4000000
-      arr.push(arr[-1] + arr[-2])
+    while arr.last(2).sum < 4000000
+      arr.push(arr.last(2).sum)
     end
-    sum = arr.reduce(0) do |sum, num|
+    arr.reduce(0) do |sum, num|
       sum += num if num.even?
       sum
     end
