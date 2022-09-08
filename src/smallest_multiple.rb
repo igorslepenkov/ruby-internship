e31cfd14950aa82bb3459477eb58523617147eb4
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-08 14:38:48 +0300

    refactor(smallest-multiple): add ? to 'is_dev_by_every', use to_a on range

--- src/smallest_multiple.rb
+++ src/smallest_multiple.rb
@@ -1,15 +1,15 @@
 module SmallestMultiple
-  def self.is_dev_by_every(number, arr)
+  def self.is_dev_by_every?(number, arr)
     !arr.any? {|num| number % num != 0}
   end
 
   def self.get_smallest_multiple_for(max_number_in_range)
-    range_arr = (1..max_number_in_range).reduce([]) {|arr, num| arr.push(num)}
+    range_arr = (1..max_number_in_range).to_a
     max_possible_cm = range_arr.reduce(:*)
     multiple = max_number_in_range
     (1..max_possible_cm).each do |i|
       multiple = max_number_in_range * i
-      break if self.is_dev_by_every(multiple, range_arr)
+      break if self.is_dev_by_every?(multiple, range_arr)
     end
     multiple
   end
