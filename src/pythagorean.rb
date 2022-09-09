4638d5275ce595b2e3eda62d2c04cbb5d95f0c7c
igorslepenkov <slepenkov.nii@yandex.by>
2022-09-09 13:09:27 +0300

    refactor(pythagorean-triplet): addd space around operators, use destructuring, change vars in block for sort

--- src/pythagorean.rb
+++ src/pythagorean.rb
@@ -1,15 +1,13 @@
 module Pythagorean
   def self.right_triangle?(a, b, c)
-    sides = [a, b, c].sort{|a, b| b <=> a}
-    hyp = sides[0]
-    first_leg = sides[1]
-    second_leg = sides[2]
+    sides = [a, b, c].sort{|i, j| j <=> i}
+    hyp, first_leg, second_leg = sides
     first_leg.pow(2) + second_leg.pow(2) == hyp.pow(2)
   end
 
   def self.find_triplet_product_from(sum)
-    (1..(sum/3).floor).each do |a| 
-      (a..(sum/2).floor).each do |b|
+    (1..(sum / 3).floor).each do |a| 
+      (a..(sum / 2).floor).each do |b|
         c = sum - a - b
         return a * b * c if self.right_triangle?(a, b, c)
       end
